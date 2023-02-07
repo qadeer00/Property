ac1c09536485ce4696c8d2600e82729d31697473
Qadeer <abdulqadeer0120@gmail.com>
2023-02-07 19:44:08 +0500

    auth issue

--- resources/views/sidebar.blade.php
+++ resources/views/sidebar.blade.php
@@ -7,52 +7,62 @@
                 <a class="sidebar-link waves-effect waves-dark sidebar-link" href="dashboard.html"
                     aria-expanded="false">
                     <i class="far fa-clock" aria-hidden="true"></i>
-                    <span class="hide-menu">Dashboard</span>
+                    <span class="hide-menu">Private Data Entry</span>
                 </a>
             </li>
             <li class="sidebar-item">
                 <a class="sidebar-link waves-effect waves-dark sidebar-link" href="profile.html"
                     aria-expanded="false">
                     <i class="fa fa-user" aria-hidden="true"></i>
-                    <span class="hide-menu">Profile</span>
+                    <span class="hide-menu">Private Data Entry</span>
                 </a>
             </li>
             <li class="sidebar-item">
                 <a class="sidebar-link waves-effect waves-dark sidebar-link" href="basic-table.html"
                     aria-expanded="false">
                     <i class="fa fa-table" aria-hidden="true"></i>
-                    <span class="hide-menu">Basic Table</span>
+                    <span class="hide-menu">For Sale Availability</span>
                 </a>
             </li>
             <li class="sidebar-item">
                 <a class="sidebar-link waves-effect waves-dark sidebar-link" href="fontawesome.html"
                     aria-expanded="false">
                     <i class="fa fa-font" aria-hidden="true"></i>
-                    <span class="hide-menu">Icon</span>
+                    <span class="hide-menu">For Rent Availability</span>
                 </a>
             </li>
             <li class="sidebar-item">
                 <a class="sidebar-link waves-effect waves-dark sidebar-link" href="map-google.html"
                     aria-expanded="false">
                     <i class="fa fa-globe" aria-hidden="true"></i>
-                    <span class="hide-menu">Google Map</span>
+                    <span class="hide-menu"> Total Users</span>
                 </a>
             </li>
             <li class="sidebar-item">
                 <a class="sidebar-link waves-effect waves-dark sidebar-link" href="blank.html"
                     aria-expanded="false">
                     <i class="fa fa-columns" aria-hidden="true"></i>
-                    <span class="hide-menu">Blank Page</span>
+                    <span class="hide-menu">Users</span>
                 </a>
             </li>
             <li class="sidebar-item">
                 <a class="sidebar-link waves-effect waves-dark sidebar-link" href="404.html"
                     aria-expanded="false">
                     <i class="fa fa-info-circle" aria-hidden="true"></i>
-                    <span class="hide-menu">Error 404</span>
+                    <span class="hide-menu">User Type</span>
                 </a>
             </li>
            
+            <li class="sidebar-item">
+                
+                <a class="sidebar-link waves-effect waves-dark sidebar-link" href="404.html"aria-expanded="false">
+                   
+                        <i class="fa fa-sign-out" aria-hidden="true"></i>  
+                    
+                   
+                    <span class="hide-menu">Sign Out</span>
+                </a>
+            </li>
         </ul>
 
     </nav>
