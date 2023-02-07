c4afeffb1e351e22b54f33f970500b002a335025
Qadeer <abdulqadeer0120@gmail.com>
2023-02-07 22:55:57 +0500

    view

---
+++ database/migrations/2023_02_07_170101_create_propertytables_table.php
@@ -0,0 +1,52 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('propertytables', function (Blueprint $table) {
+            $table->id();
+            $table->string('location');
+            $table->string('property');
+            $table->enum('property_type', ['Villa', 'Apartment', 'TownHouse']);
+            $table->integer('floor_no');
+            $table->integer('unit_no');
+            $table->string('bhk');
+            $table->string('view');
+            $table->string('size');
+            $table->string('price');
+            $table->string('comment');
+            $table->string('calling_date');
+            $table->enum('availability_status', ['For Sale', 'Not Available', 'For Rent']);
+            $table->string('is_post');
+           
+            $table->enum('status', ['Vacant', 'Occupied']);
+            $table->string('client_name');
+            $table->string('contact_no');
+            $table->string('email');
+            $table->string('assignedTo');
+            $table->string('assignedBy');
+          
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('propertytables');
+    }
+};
