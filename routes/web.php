a45ff61654f7925e46ee237bbccd517d2573b900
Qadeer <abdulqadeer0120@gmail.com>
2023-02-07 18:44:30 +0500

    login issue

--- routes/web.php
+++ routes/web.php
@@ -18,13 +18,18 @@ Route::get('/', function () {
     return view('signin');
 });
 Route::get('master', function () {
+<<<<<<< HEAD
     return view('master')->middleware('admin_role');
 });
+=======
+    return view('master');
+})->middleware('admin_role');
+>>>>>>> 81b2e8c73c4fa3ec75d2e444ce8cd867de5e95dc
 
 Auth::routes();
 
-    Route::get('admin/home', [App\Http\Controllers\HomeController::class, 'adminHome'])->name('admin.home')->middleware('admin_role');
-Route::get('agent/home', [App\Http\Controllers\HomeController::class, 'agentHome'])->name('agent.home')->middleware('user_role');
+    Route::get('admin/home', [App\Http\Controllers\HomeController::class, 'adminHome'])->middleware('admin_role');
+Route::get('agent/home', [App\Http\Controllers\HomeController::class, 'agentHome'])->middleware('user_role');
 
 
 //Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home')->name('admin.home')->middleware('Auth','user_role');
